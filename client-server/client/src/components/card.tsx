274b0af1b26c77c18161edc9451a3fcff550a60b
amina-gbc <amina27gabeljic@gmail.com>
2023-05-07 21:48:17 +0200

    added profile (newest version)

---
+++ client-server/client/src/components/card.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import '../styles/card.css';
+import flame from '../img/flame.png';
+
+const Card = () => {
+    return (
+        <div className="activityTitle mb-6">
+            <div className="cardTop">
+                <h1 className="text-lg ml-4 mt-2">Running</h1>
+                <img src={flame} width="40" height="30" />
+            </div>
+            <div className="cardBottom">
+                <div>
+                    <div className="circle"></div>
+                    <h3 className="mb-4">Current Streak</h3>
+                </div>
+                <div>
+                    <div className="circle"></div>
+                    <h3 className="mb-4">Highest Streak</h3>
+                </div>
+                <div>
+                    <div className="circle"></div>
+                    <h3 className="mb-4">Point Multiplier</h3>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
--- client-server/client/src/pages/profile/profile.tsx
+++ client-server/client/src/pages/profile/profile.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 
 import './userProfile.css';
+import { Box, LinearProgress, ToggleButton, ToggleButtonGroup, Typography } from '@mui/material';
+import Card from '../../components/card';
+import Layout from '../../layout/loggedIn/layout';
 
 export interface User {
   id: number,
@@ -26,43 +29,61 @@ const UserProfile = () => {
 
   // pointsBar.style.width = '20%';
 
+  //BUTTONS
+  const [alignment, setAlignment] = React.useState('web');
 
+  const handleChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newAlignment: string,
+  ) => {
+    setAlignment(newAlignment);
+  };
+
+  //PROGRESS BAR
+  const [progress, setProgress] = React.useState(50);
 
   return (
+    <Layout>
+      <div className="profileArea1">
+        <div className="profileTable1">
+          <div className="row">
+            <div className="profile-nav col-md-3">
+              <div className="panel">
+                <div className="user-heading round">
+                  <a href="#">
+                    <img src="https://i.pinimg.com/564x/75/90/4d/75904d968f8ee57bb0e1fd28f3424a53.jpg" alt="Profile-Picture"/>
+                  </a>
+                  <h1>Hog Rider</h1>
+                  <p>hogrider@gmail.com</p>
 
-    <div className="profileArea1">
-      <div className="profileTable1">
-        <div className="row">
-          <div className="profile-nav col-md-3">
-            <div className="panel">
-              <div className="user-heading round">
-                <a href="#">
-                  <img src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="Profile-Picture" />
-                </a>
-                <h1>Amina Gabeljic</h1>
-                <p>amina27gabeljic@gmail.com</p>
+                  <div className="profileButtons">
+                    <ToggleButtonGroup
+                      value={alignment}
+                      exclusive
+                      onChange={handleChange}
+                      aria-label="Platform"
+                    >
+                      <ToggleButton value="web">Stats</ToggleButton>
+                      <ToggleButton value="android">Settings</ToggleButton>
+                    </ToggleButtonGroup>
+                  </div>
 
-                <ul className="profileTable">
-                  <li><a href="#" className='profileButton'>Stats</a></li>
-                  <li><a href="#" className='profileButton'>Settings</a></li>
-                </ul>
-
-                <div className="points-bar">
-                  <div className="bar"></div>
+                  <div className="mt-8">
+                    <h3 className="text-left mb-3">Level 01</h3>
+                    <Box sx={{ width: "100%", color: "#FF0000" }}>
+                      <LinearProgress className="mb-12" variant="buffer" value={progress} />
+                    </Box>
+                  </div>
+                  <Card />
+                  <Card />
+                  <Card />
                 </div>
               </div>
             </div>
           </div>
-
-          <div className="rowCurrentActivity">
-            <div className="activityTitle">
-              Running<img src="https://bootdey.com/img/Content/avatar/avatar3.png" width="20" height="20" className="activityImage" />
-            </div>
-          </div>
-
         </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
