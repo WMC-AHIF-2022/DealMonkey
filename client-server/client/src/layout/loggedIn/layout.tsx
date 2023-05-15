d178834ef3dc2dfd1e208e382e8af2a5ed58b2e2
Christian-Ekhator <christian.ekhator06@gmail.com>
2023-05-15 13:34:09 +0200

    navbar position fixed

--- client-server/client/src/layout/loggedIn/layout.tsx
+++ client-server/client/src/layout/loggedIn/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from "./navbar";
 
 const LoggedInLayout = ({ children }: any) => {
   return (
-    <div>
+    <div className="wrapper">
       <Navbar />
       <div className="font mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 lg:mt-8">
         {children}
